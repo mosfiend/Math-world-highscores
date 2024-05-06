469e84dbf99b12dc5e7e70ca6652ae14d04816c8
mosfiend <abdellaouimoss@gmail.com>
2024-05-06 23:12:27 +0100

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 
-app = express();
+const app = express();
 app.use(express.json());
 
 /////////// CONNECT TO Database
