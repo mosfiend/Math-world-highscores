bdbf4c3d903904c15ab82977eadb297841f1fc3b
mosfiend <abdellaouimoss@gmail.com>
2024-06-29 20:50:39 +0100

    disabled autoindexing

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const connection = process.env.MONGO_URI;
 mongoose
-  .connect(connection)
+  .connect(connection, { autoIndex: false })
   .then(() => console.log("Database Connected Successfully"))
   .catch((err) => console.log("bruh", err));
